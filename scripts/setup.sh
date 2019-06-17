9647fcdd3e5161c94126d0d7df8bdd8d347c17ab
Julien Brochet <julien@aerialls.io>
2019-06-17 22:54:38 +0200

    feat(script): use a dedicated sudoer file

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,7 @@
 set -eux
 
 # Enable "madalynn" user sudo without password
-echo "madalynn  ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo "madalynn ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/madalynn
 
 # Remove the legacy password
 passwd --delete madalynn
