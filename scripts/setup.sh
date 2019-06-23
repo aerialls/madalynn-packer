af29d38252dffca8826b5a403f05f5e490481b71
Julien Brochet <julien@aerialls.io>
2019-06-23 13:01:10 +0200

    feat(proxmox): improve template for Ubuntu 19.04

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,3 @@ set -eux
 
 # Enable "madalynn" user sudo without password
 echo "madalynn ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/madalynn
-
-# Remove the legacy password
-passwd --delete madalynn
